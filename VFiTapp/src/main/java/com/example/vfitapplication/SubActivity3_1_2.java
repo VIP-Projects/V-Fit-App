cc5f729463f7c9abeb8fe1b23fb2df836d702692
Dayoung Kil <53934639+arittung@users.noreply.github.com>
2023-08-25 11:14:52 +0900

    Add files via upload

--- VFiTapp/src/main/java/com/example/vfitapplication/SubActivity3_1_2.java
+++ VFiTapp/src/main/java/com/example/vfitapplication/SubActivity3_1_2.java
@@ -23,6 +23,16 @@ public class SubActivity3_1_2 extends AppCompatActivity {
         ImageButton recommend7 = findViewById(R.id.recommend7);
         ImageButton recommend8 = findViewById(R.id.recommend8);
 
+        recommend1.setImageResource(R.drawable.cloth1);
+        recommend2.setImageResource(R.drawable.cloth2);
+        recommend3.setImageResource(R.drawable.cloth3);
+        recommend4.setImageResource(R.drawable.cloth4);
+        recommend5.setImageResource(R.drawable.cloth5);
+        recommend6.setImageResource(R.drawable.cloth2);
+        recommend7.setImageResource(R.drawable.cloth3);
+        recommend8.setImageResource(R.drawable.cloth4);
+
+
         recommend1.setOnClickListener(new View.OnClickListener() {    // 다음페이지로 넘어가는 버튼
             @Override
             public void onClick(View view) {
