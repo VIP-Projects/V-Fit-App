69abd3f14a178218c6b33dbb77fa82462cdb69b2
Dayoung Kil <53934639+arittung@users.noreply.github.com>
2023-09-04 18:35:07 +0900

    Update SubActivity4.java

--- app/src/main/java/com/example/vfitapplication/SubActivity4.java
+++ app/src/main/java/com/example/vfitapplication/SubActivity4.java
@@ -35,8 +35,8 @@ public class SubActivity4 extends AppCompatActivity {
         Button useruploadbtn = findViewById(R.id.userupload);   // 사용자 이미지 업로드 버튼
         ImageButton nextbtn3 = findViewById(R.id.nextbtn3);     // 다음페이지로 넘어가는 버튼
 
-
-        useruploadbtn.setOnClickListener(new View.OnClickListener() {   // 사용자 이미지 업로드 클릭 시 갤러리 이동
+        // 사용자 이미지 업로드 클릭 시 갤러리 이동
+        useruploadbtn.setOnClickListener(new View.OnClickListener() {   
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_PICK);
@@ -44,8 +44,9 @@ public class SubActivity4 extends AppCompatActivity {
                 startActivityForResult(intent, 1);
             }
         });
-
-        nextbtn3.setOnClickListener(new View.OnClickListener() {    // 다음페이지로 넘어가는 버튼
+        
+       // 다음페이지로 넘어가는 버튼
+        nextbtn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ByteArrayOutputStream stream = new ByteArrayOutputStream();
@@ -75,36 +76,13 @@ public class SubActivity4 extends AppCompatActivity {
                     InputStream instream = resolver.openInputStream(uri);
                     imgBitmap = BitmapFactory.decodeStream(instream);
                     instream.close();   // 스트림 닫아주기
-
-                    //UserImagePath = getRealPathFromURI(uri);     // imagePath에 string으로 경로 저장
-
-                    userpath.setText("Successful Upload of File");
+                    userpath.setText("Successful Upload of File");    // 현재 페이지에 갤러리 업로드 상태 메시지 출력
                 } catch (Exception e){
-                    userpath.setText("Failed to Upload file");
+                    userpath.setText("Failed to Upload file");    // 현재 페이지에 갤러리 업로드 상태 메시지 출력
                 }
             }
 
         }
     }
 
-//    갤러리 사진 참고: https://machine-woong.tistory.com/91 / https://jeongchul.tistory.com/287
-//    사진 url 띄우기 참고 : https://jjyloves.tistory.com/13 / https://wikidocs.net/99371
-
-
-    // 이미지의 절대 경로를 구해주는 함수 (참고: https://hhhhhhhong.tistory.com/28)
-    private String getRealPathFromURI(Uri contentURI) {
-        String result;
-        Cursor cursor = getContentResolver().query(contentURI, null, null, null, null);
-
-        if (cursor == null) { // Source is Dropbox or other similar local file path
-            result = contentURI.getPath();
-        }
-        else {
-            cursor.moveToFirst();
-            int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-            result = cursor.getString(idx);
-            cursor.close();
-        }
-        return result;
-    }
 }
