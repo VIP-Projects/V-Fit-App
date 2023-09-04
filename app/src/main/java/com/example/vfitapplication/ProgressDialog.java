39f3ae5d3a93d55ccb282d02953b0a7bb2ddb82c
Dayoung Kil <53934639+arittung@users.noreply.github.com>
2023-09-04 16:53:13 +0900

    Update ProgressDialog.java

--- app/src/main/java/com/example/vfitapplication/ProgressDialog.java
+++ app/src/main/java/com/example/vfitapplication/ProgressDialog.java
@@ -4,6 +4,7 @@ import android.app.Dialog;
 import android.content.Context;
 import android.view.Window;
 
+// 가상 피팅 실행 과정 중 나타나는 로딩 창
 
 public class ProgressDialog extends Dialog{
     public ProgressDialog(Context context){
