4d3718d04d4e0f6b353479875fa72efb355c09a5
Dayoung Kil <53934639+arittung@users.noreply.github.com>
2023-09-04 18:31:09 +0900

    Update SubActivity3_1_1.java

--- app/src/main/java/com/example/vfitapplication/SubActivity3_1_1.java
+++ app/src/main/java/com/example/vfitapplication/SubActivity3_1_1.java
@@ -18,22 +18,21 @@ public class SubActivity3_1_1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub311);
-//        TextView textUserImage = findViewById(R.id.textUserImage);  // 유저 이미지
-//        TextView textCloth =  findViewById(R.id.textCloth);  // 옷 이미지
+
         Intent intent = getIntent();
 
-        // 이미지 경로 이용해 이미지 가져오는 코드 참고 : https://sharp57dev.tistory.com/22
+        // 이미지 경로 이용해 이미지 가져오는 코드 (참고: https://sharp57dev.tistory.com/22)
         ImageView vfit_result = findViewById(R.id.vfit_result);
         vfit_result.setImageResource(R.drawable.iu_result);
 
-        //textUserImage.setText(intent.getStringExtra("UserImage").toString());
-        //textCloth.setText(intent.getStringExtra("ClothImage").toString());
         String resultimage = intent.getStringExtra("resultImage").toString();
         byte[] encodeByte = android.util.Base64.decode(resultimage, android.util.Base64.DEFAULT);
         Bitmap result_bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
-        vfit_result.setImageBitmap(result_bitmap);
+        vfit_result.setImageBitmap(result_bitmap);    // 결과 이미지 출력
 
+        // 화면 전체가 다음 페이지로 이동할 수 있는 이미지 버튼
         ImageButton imgbtn = findViewById(R.id.imagebtn2);
+        // 화면 어느 곳이든, 클릭 시 다음 페이지로 이동
         imgbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
